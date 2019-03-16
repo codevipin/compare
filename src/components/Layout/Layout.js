df0b19b5e18282f34273756d8bf1a07ffaef34d1
codevipin <vipinverma@live.com>
2019-03-16 19:37:34 +0530

    add basic layout to app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+
+const Layout = ( props ) => {
+	return (
+		<Aux>
+			<div>Filters</div>
+			<div>Compare table</div>
+		</Aux>
+	)
+}
+
+export default Layout;
